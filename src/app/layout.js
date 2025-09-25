ba6e3710e9232539890223f132ee42ae1195ad6c
itstahirrafique <121024103+tahir-rafique@users.noreply.github.com>
2025-09-25 14:25:40 +0500

    initalized a new next.js project

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./style/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Title here",
+  description: "Write meta description here",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+import Button from "@/components/shared/button";
+import Input from "@/components/shared/input";
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <section className="h-[300vh]  scrollbar-hidden">
+        <div className="container">
+          <Input
+            label="First Name"
+            placeholder="Enter your name"
+            errorMessage={10}
+            type="date"
+          />
+          <Button text="red" className="text-9xl" />
+        </div>
+      </section>
+    </>
+  );
+}
